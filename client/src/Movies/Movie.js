50ce35a878a3c5c74f56de7b4267a8bf4b3d3c8b
Joshua Agins <56888002+jagins@users.noreply.github.com>
2019-12-17 14:56:41 -0600

    Removed redunant code from files and repalced it with MovieCard component nothing is broken and working fine

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
   const { slug } = useParams();
-  
+
   useEffect(() => {
     const id = slug;
     // change ^^^ that line and grab the id from the URL
@@ -35,22 +36,11 @@ const Movie = (props) => {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard 
+        title={title}
+        director={director}
+        metascore={metascore}
+        stars={stars}/>
       <div className="save-button">Save</div>
     </div>
   );
